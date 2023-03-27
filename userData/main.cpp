6f04b212e7f6a02a4d9cdb98c8df6e6b0526d475
H-4-P-P-Y <99682174+H-4-P-P-Y@users.noreply.github.com>
2023-03-27 10:26:06 +0200

    genPass

--- userData/main.cpp
+++ userData/main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
-
-struct Informacje {
-    string imie;
-    string nazwisko;
-    string stanowisko;
-};
 
 void pobierzDane(Informacje& informacje) {
     cout << "Podaj imie: ";
@@ -38,12 +34,68 @@ void wypiszStanowisko(string stanowisko) {
     }
 }
 
+string generujHaslo(int dlugoscHasla, bool duzeLitery, bool cyfry, bool znakiSpecjalne) {
+    srand(time(NULL));
+    char haslo[dlugoscHasla] = {};
+    char maleLiteryAlfabet[25] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    char duzeLiteryAlfabet[25] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char cyfryAlfabet[10] = {'0','1','2','3','4','5','6','7','8','9'};
+    char znakiSpecjalneAlfabet[14] = {'!','@','#','$','%','^','&','*','(',')','_','+','-','='};
+    string hasloStr = "";
+
+    for(int i=0;i<dlugoscHasla;i++){
+        haslo[i] = maleLiteryAlfabet[(rand()%(25-0+1))+0];
+    }
+
+    if (duzeLitery) {
+        int losId = (rand()%(dlugoscHasla-0+1))+0
+        haslo[losId] = duzeLiteryAlfabet[(rand()%(25-0+1))+0];
+    }
+    if (cyfry) {
+        int losId = (rand()%(dlugoscHasla-0+1))+0
+        haslo[losId] = cyfryLiteryAlfabet[(rand()%(10-0+1))+0];
+    }
+    if (znakiSpecjalne) {
+        int losId = (rand()%(dlugoscHasla-0+1))+0
+        haslo[losId] = znakiSpecjalneAlfabet[(rand()%(14-0+1))+0];
+    }
+
+    for(int i = 0; i<dlugoscHasla ;++){
+        hasloStr += haslo[i];
+    }
+
+    return hasloStr;
+
+}
+
 int main() {
-    Informacje informacje;
+    string imie;
+    string nazwisko;
+    string stanowisko;
+
     pobierzDane(informacje);
-    cout << "Imie: " << informacje.imie << endl;
-    cout << "Nazwisko: " << informacje.nazwisko << endl;
     cout << "Stanowisko: ";
     wypiszStanowisko(informacje.stanowisko);
+
+    cout << "Imie: " << informacje.imie << endl;
+    cout << "Nazwisko: " << informacje.nazwisko << endl;
+
+    int dlugoscHasla ;
+    bool maleLitery, duzeLitery, cyfry, znakiSpecjalne;
+
+    cout << "Podaj dlugosc hasla: ";
+    cin >> dlugoscHasla;
+
+    cout << "Czy haslo ma zawierac male litery? (1-Tak, 0-Nie): ";
+    cin >> maleLitery;
+    cout << "Czy haslo ma zawierac duze litery? (1-Tak, 0-Nie): ";
+    cin >> duzeLitery;
+    cout << "Czy haslo ma zawierac cyfry? (1-Tak, 0-Nie): ";
+    cin >> cyfry;
+    cout << "Czy haslo ma zawierac znaki specjalne? (1-Tak, 0-Nie): ";
+    cin >> znakiSpecjalne;
+
+
+
     return 0;
-}
+    }
